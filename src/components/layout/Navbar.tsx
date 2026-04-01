b4300aee9f95c974fa3328c91c6644a5b3a8f60d
Piyush Shrivastava <piyushshrivastava22ec27@gmail.com>
2026-04-01 22:27:17 +0530

    intergrated link to navigate to particular section

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -5,14 +5,21 @@ const Navbar = () => {
             <nav className="w-full h-14 bg-black text-green-400 flex items-center justify-between px-60 font-mono sticky top-0 z-50">
             
             <div className="text-lg cursor-pointer hover:drop-shadow-[0_0_10px_rgba(34,240,94,1)] ">
-                &gt;_ piyush.dev
+                <a href="#">&gt;_ piyush.dev</a>
             </div>
 
-            <div className="flex items-center gap-8 text-sm text-white">
+            {/* <div className="flex items-center gap-8 text-sm text-white">
                 <span className="cursor-pointer hover:text-green-300">/about</span>
                 <span className="cursor-pointer hover:text-green-300">/skills</span>
                 <span className="cursor-pointer hover:text-green-300">/projects</span>
                 <span className="cursor-pointer hover:text-green-300">/contact</span>
+            </div> */}
+
+            <div className="flex items-center gap-8 text-sm text-white">
+                <a href="#about" className="cursor-pointer hover:text-green-300">/about</a>
+                <a href="#skills" className="cursor-pointer hover:text-green-300">/skills</a>
+                <a href="#projects" className="cursor-pointer hover:text-green-300">/projects</a>
+                <a href="#contact" className="cursor-pointer hover:text-green-300">/contact</a>
             </div>
 
             </nav>
